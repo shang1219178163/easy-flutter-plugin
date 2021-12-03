40ee3f6430d3e49b34e17dbbf5074d600532115b
shangbinbin <shangbinbin@haier.com>
2021-12-03 13:32:39 +0800

    update

--- src/utils/string-extensions.ts
+++ src/utils/string-extensions.ts
@@ -13,25 +13,43 @@ declare global {
 	interface String {
 		format(...args: string[]): string;
 
+         /**
+         * Gets a substring beginning at the specified location and having the specified length.
+         * @param from The starting position of the desired substring. The index of the first character in the string is zero.
+         * @param length The number of characters to include in the returned substring.
+         */
+        range(location: number, length?: number): string;
+
 		isEmpty(): boolean;
+        ///字符串翻转
+        reverse(): string;
+
 		/// 首字母大写
 		toCapitalized(): string;
         /// 获取两字符中间的部分
-		substringBetween(prefix: string, suffix: string, isContain: boolean): string;
+		substringBetween(prefix: string, suffix: string, isContain?: boolean): string;
         /// 驼峰命名法
-        camelCase(splitter: string, firstCharacterCapitalize: boolean): string;
+        camelCase(separator: string, isUpper?: boolean): string;
         /// 反驼峰命名法
-        uncamelCase(splitter: string): string;
+        uncamelCase(separator?: string): string;
 	}
 
 }
 export {}; 
 
 if (!String.prototype.format) { 　　　　
-    String.prototype.format = function (...args: string[]): string {
-        return this.replace(/{(\d+)}/g, function (match, number) {
-            return (typeof args[number] !== 'undefined') ? args[number] : match;
-        });
+    String.prototype.format = function(...args: string[]): string {
+        var s = this.toString();
+        for(var i = 0; i < args.length; i++){
+            s = s.replace(new RegExp("\\{"+i+"\\}","g"), args[i]);
+        }
+        return s;
+    };
+}
+
+if (!String.prototype.range) { 　　　　
+    String.prototype.range = function(location: number, length?: number): string {
+        return this.substr(location, length);
     };
 }
 
@@ -41,14 +59,21 @@ if (!String.prototype.isEmpty) {
     };
 }
 
+if (!String.prototype.reverse) {
+    String.prototype.reverse = function (): string {
+        return this.split("").reverse().join("");
+    };
+}
+
 if (!String.prototype.toCapitalized) {
     String.prototype.toCapitalized = function (): string {
-        return this[0].toUpperCase() + this.substr(1);
+        // return this[0].toUpperCase() + this.substr(1);
+        return this.substr(0, 1).toUpperCase() + this.substr(1);
     };
 }
 
 if (!String.prototype.substringBetween) {
-    String.prototype.substringBetween = function(prefix: string, suffix: string, isContain: boolean = false): string {
+    String.prototype.substringBetween = function(prefix: string, suffix: string, isContain?: boolean): string {
         const startIdx = this.indexOf(prefix);
         const endIdx = this.lastIndexOf(suffix);
         if (isContain === true) {
@@ -60,11 +85,10 @@ if (!String.prototype.substringBetween) {
 }
 
 if (!String.prototype.camelCase) {
-    String.prototype.camelCase = function(separator: string = "-", firstCharacterCapitalize: boolean = true): string {
-    // String.prototype.camelCase = function(separator: string, firstCharacterCapitalize: boolean = true): string {
+    String.prototype.camelCase = function(separator: string, isUpper?: boolean): string {
         return this.split(separator)
         .map((e, index) => {
-            if (index === 0 && firstCharacterCapitalize === false) {
+            if (index === 0 && isUpper !== true) {
                 return e;
             }
             return e.toCapitalized();
@@ -74,10 +98,13 @@ if (!String.prototype.camelCase) {
 }
 
 if (!String.prototype.uncamelCase) {
-    String.prototype.uncamelCase = function(separator: string = "-"): string {
+    String.prototype.uncamelCase = function(separator?: string): string {
         return this.split("")
-        .map(e => {
-            return /[A-Z]/.test(e) ? `${separator}${e.toLowerCase()}` : e;
+        .map((e, index) => {
+            if (index === 0) {
+                return /[A-Z]/.test(e) ? e.toLowerCase() : e;
+            }
+            return /[A-Z]/.test(e) ? `${separator ?? "_"}${e.toLowerCase()}` : e;
         })
         .join("");
     };
@@ -89,11 +116,19 @@ export {};
 
 
 /// interface 方法测试
-function test(){
+export function testString(){
+
+    //方式1
+    var test = '我的{0}是{1}!{2}';
+    var result = test.format('id','城市之光', "Ye!");
+    console.log(result);
+
     const line = "static Future<String> getAppVersion4(double val) async {";
-    let name = line.substringBetween("> ", "(", false);
+    let name = line.substringBetween("> ", "(",);
     console.log(name);
 
+    let name1 = line.substringBetween("> ", "(", false);
+    console.log(name1);
 
     let a = "hello_test_one".camelCase("_", true);
     console.log(`camelCase: ${a}`);
@@ -101,7 +136,11 @@ function test(){
     let a1 = "hello_test_one".camelCase("_", false);
     console.log(`camelCase1: ${a1}`);
 
-    let a2 = "hello_test_one".uncamelCase("_");
+    let a2 = a.uncamelCase("-");
     console.log(`uncamelCase: ${a2}`);
 
+    let a22 = a1.uncamelCase();
+    console.log(`uncamelCase22: ${a22}`);
+
+    "".
 }
